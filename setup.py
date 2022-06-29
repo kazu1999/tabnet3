9ea9cc21bbc424f770676b7d624172e4a847d549
松本和人 <kazuto@matsumotokazutonoMacBook-ea.local>
2022-06-29 16:37:30 +0900

    コミットメッセージ

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+
+#!usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name="pig",
+	version="0.1",
+	description="Test for pip install git+",
+	url="",
+	packages=find_packages(),
+	entry_points="""
+	[console_scripts]
+	pig = pig.pig:main
+	""",
+	)
